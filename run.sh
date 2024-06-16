e70636f7611e5b2e5c375d33a928a9c4a2e1c634
Andrew Miller <socrates1024@gmail.com>
2024-06-16 15:25:01 +0000

    noninteractive for docker

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ touch untrustedhost/request.csr
 	    --preferred-challenges http --http-01-port 8082 \
 	    --csr untrustedhost/request.csr \
 	    --fullchain-path untrustedhost/certificate.pem \
+	    --non-interactive --agree-tos -m soc1024@illinois.edu \
 	    --config-dir certbot --work-dir certbot --logs-dir certbot
 } &
 gramine-sgx python
